0d80a6d1fa042b764360140d3bd0e37cf35cc499
Joshua Hill <josh@jlhworkshop.com>
2019-07-31 18:34:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,3 +5,27 @@
 #  return source_array.map {|n| n ** 2}
 # end
 
+def map(s)
+  new = []
+  i = 0
+  while i < s.length
+    new.push(yield(s[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
