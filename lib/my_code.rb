0207de01545ad3aa57536e2c1f185cd457f61f28
Joshua Hill <josh@jlhworkshop.com>
2019-07-31 18:33:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,6 @@
 def my_own_map(source_array)
   return map(source_array) {|n| n * -1}
   return source_array.map {|n| n * 1}
-  return source_array.map { |n| n * 2}  
+  return source_array.map {|n| n * 2}  
   return source_array.map {|n| n ** 2}
 end
